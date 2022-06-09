311a28a5352f36b3401309020b24ed1f0cde51e9
Lisa Fink <lisadfink@gmail.com>
2022-06-09 12:53:14 -0400

    Add multiple schools to content

---
+++ src/components/cv/SchoolCv.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class SchoolCv extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    const inputs = this.props.inputs;
+    const index = this.props.index;
+    return (
+      <div id="education">
+        <div id="school">{inputs.education[index].school}</div>
+        <div>Degree Earned: {inputs.education[index].degree}</div>
+        <div>Major: {inputs.education[index].major}</div>
+        <div>Graduated: {inputs.education[index].grad}</div>
+      </div>
+    );
+  }
+}
