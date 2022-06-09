ce8284ec5cf1f419ce8f9bff617296cf3fec3414
Lisa Fink <lisadfink@gmail.com>
2022-06-08 20:51:56 -0400

    Initial Commit

---
+++ src/components/Ed.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Ed extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <legend>General Info</legend>
+      </div>
+    );
+  }
+}
+
+export default Ed;
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Ed from './Ed';
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form>
+        <Ed />
+      </form>
+    );
+  }
+}
